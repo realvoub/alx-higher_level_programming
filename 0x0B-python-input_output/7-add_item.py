8fb63091f981a0c460ab6dd8810370838bec1d0e
realvoub <benjaminvictor64@yahoo.com>
2023-01-10 18:02:32 +0000

    output

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module to load and save json"""
+
+
+import sys
+
+
+if __name__ == "__main__":
+    save_to_json = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json = __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json('add_item.json')
+    except FileNotFoundError:
+        items = []
+
+    items.extend(sys.argv[1:])
+    save_to_json(items, 'add_item.json')
