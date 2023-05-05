d1e5ad0dfbeaa8d83185885d6db3a0d0fb8d4a53
realvoub <benjaminvictor64@yahoo.com>
2023-05-06 00:24:55 +0100

    webscraping

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
-
-const fileName = process.argv[2];
 const fs = require('fs');
+const inputFile = process.argv[2];
 
-fs.readFile(fileName, 'utf-8', (err, data) => {
+fs.readFile(inputFile, 'utf8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
+  } else {
+    console.log(data);
   }
-  console.log(data);
 });
